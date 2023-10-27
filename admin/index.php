f05d492f346d415dab8becd4218a9a69f6d314f0
Jaanus Nurmoja <jaanus.nurmoja@gmail.com>
2023-10-28 00:15:42 +0300

    muudetud debug

--- admin/index.php
+++ admin/index.php
@@ -3,70 +3,25 @@
 require_once 'Autoload.php';
 
 use \Controller\Table as TableController;
-use \Model\Table;
 
 $request = isset($_SERVER['PATH_INFO']) ?  explode('/', $_SERVER['PATH_INFO']) : [];
 
-echo 'Hello world!';
-echo '<hr>';
-
 $tc = new TableController();
 
-$model = new Table(null, true);
-echo $model->hello;
-echo '<hr>';
-echo 'Nüüd on uutmoodi: <hr>';
-echo '<pre>';
-print_r($tc->pathParams($request));
-echo '</pre>';
-echo '<pre>';
-print_r($tc->getTables());
-echo '</pre>';
+//echo '<pre>';
+//print_r($tc->pathParams($request));
+//echo '</pre>';
+$r = $tc->pathParams($request);
 
-/*
-$model->setId(1);
-$model->setName('table');
-$data = new Data;
-$fields = [];
-for ($i = 0; $i < 3; $i++) {
-$f = new Field;
-$f->setId($i + 1);
-$f->setName('title ' . $i + 1);
-$f->setType('string');
-$f->setHtmlDefaults('{"field":"input","type":"text"}');
-$fields[$f->getName()] = $f;
+if (isset($r[1]) && $r[1] == 'tables') {
+    echo json_encode($tc->getTables());
 }
-
-$belongsTo = [];
-$hasMany = [];
-$hasManyAndBelongsTo = [];
-$bt = [];
-$relations = [];
-
-for ($i = 0; $i < 3; $i++) {
-$relations[$i] = new Relations;
-for ($d = 0; $d < 5; $d++) {
-$bt[$d] = new RelationDetails;
-$rel = new Relation;
-$rel->setId($d + 1);
-$rel->setType('belongsTo');
-$bt[$d]->setId($d + 10);
-$bt[$d]->setRelation($rel);
-$bt[$d]->setRole('roll' . $d);
-}
-$relations[$i]->setRelationDetails($bt);
+else {
+    echo '{"request":';
+        echo json_encode($tc->pathParams($request));
+        echo ',';
+        echo '"data":';
+    echo json_encode($tc->getTables());
+    echo '}';
 
 }
-
-$cleanModel = $model->clean();
-$data->setTable($cleanModel);
-$data->setFields($fields);
-$model->setData($data);
-$model->setBelongsTo($relations[0]);
-$model->setHasMany($relations[1]);
-$model->setHasManyAndBelongsTo($relations[2]);
-echo '<hr>';
-echo '<pre>';
-print_r($model);
-echo '</pre>';
- */
