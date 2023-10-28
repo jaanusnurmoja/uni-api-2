83f3834134de8870c5076c7e5da516b82081b364
Jaanus Nurmoja <jaanus.nurmoja@gmail.com>
2023-10-28 14:50:49 +0300

    Kaks uut faili jäid enne lisamata

---
+++ admin/view/Table.php
@@ -0,0 +1,69 @@
+<?php namespace View;
+
+class Table
+{
+    public $tableSingleOrList;
+    public function __construct($tableSingleOrList)
+    {
+        $this->tableSingleOrList = $tableSingleOrList;
+    }
+    public function tableDetails()
+    {
+        echo '<h1>' . $this->tableSingleOrList->name . '</h1>';
+        echo '<table>';
+
+        foreach ($this->tableSingleOrList as $key => $value) {
+            if (!is_object($value) && !is_array($value)) {
+            echo '<tr><td>' . $key . '</td><td>' . $value  . '</td></tr>';
+            }
+            else {
+                if ($key == 'data') {
+                    foreach ($this->tableSingleOrList->data->fields as $key => $field) {?>
+                        <tr><td><?php echo $key?></td><td><ul><?php
+                        foreach ($field as $k => $v) {
+                            echo "<li>$k: $v</li>";
+                        } ?></ul></td></tr>
+                        <?php
+                    }
+                }
+            }
+        }
+    }
+
+    public function tableList() {
+        if (is_array($this->tableSingleOrList)) {
+            ?>
+
+            <h1>Tabelid</h1>
+            <table>
+                <thead>
+                    <tr>
+                    <?php 
+                        $thead = array_keys((array) new \DTO\TableDTO());
+                        foreach ($thead as $key => $value) {
+                            echo "<td>" . $key . "</td>";
+                        }
+                    ?>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php 
+                        foreach ($this->tableSingleOrList as $row) {
+                            echo "<tr>";
+                            foreach ($row as $key => $value) {
+                                if (is_array($value)) {
+                                    $value = count($value);
+                                }
+                                echo '<td>'. $value .'</td>';
+                            }
+                            echo "</tr>";
+                        }
+                    
+                    ?>
+                </tbody>
+<?php
+
+        }
+    }
+}
+
