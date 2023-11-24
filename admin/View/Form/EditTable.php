cc4c7866881a0e6350ebdb13f2b1a98f637a7a1d
Jaanus Nurmoja <jaanus.nurmoja@gmail.com>
2023-11-25 00:21:26 +0200

    Muutmisvorm valmis (välimus ja korduvväljad)

--- admin/View/Form/EditTable.php
+++ admin/View/Form/EditTable.php
@@ -4,8 +4,9 @@ class EditTable
 {
     public $relations;
     public $data;
-    
-    public function __construct($data = null) {
+
+    public function __construct($data = null)
+    {
         $readRels = new \Controller\Table();
         $list = new \DTO\ListDTO();
         $readRels->getRelationsList($list);
@@ -15,7 +16,9 @@ class EditTable
 
     public function editTableForm($data = null)
     {
-        if (empty($data)) $data = $this->data;
+        if (empty($data)) {
+            $data = $this->data;
+        }
         ?>
 <h1>
     <?php echo $data->name ?>
@@ -29,7 +32,7 @@ class EditTable
                 if (!is_object($value) && !is_array($value) && $key != 'id') { ?>
     <label> <?php echo $key?><input type="text" name="<?php echo $key?>" value="<?php echo $value?>" /></label>
     <?php } else {
-                        if ($key == 'data') {?>
+        if ($key == 'data') {?>
     <h2>Andmeväljad</h2>
     <table class="table table-warning table-striped table-sm wrapper">
         <thead>
@@ -47,25 +50,25 @@ class EditTable
                 <td>
                     <fieldset>
                         <?php foreach ($f0 as $k0 => $v0) {
-                                        if ($k0 == 'id') {
-            
-                                            // $fKey
-                                            echo "<input type='hidden' name='table[data][fields][{{row-count-placeholder}}][$k0]' id='$k0' value='$v0' /> ";
-                                        } else {
-                                            echo "<label for='$k0'>$k0</label> <input name='table[data][fields][{{row-count-placeholder}}][$k0]' id='$k0'";
-                                            if (is_bool($v0)) {
-                                                $checked = $v0 ? ' checked="checked"' : '';
-                                                echo " type='checkbox' value=true$checked onclick=this.toggleAttribute('checked') />";
-                                            } else {
-                                                echo " type='text' value='$v0' />";
-                                            }
-                                        }
-                                    }?>
+                            if ($k0 == 'id') {
+
+                                // $fKey
+                                echo "<input type='hidden' name='table[data][fields][{{row-count-placeholder}}][$k0]' id='$k0' value='$v0' /> ";
+                            } else {
+                                echo "<label for='$k0'>$k0</label> <input name='table[data][fields][{{row-count-placeholder}}][$k0]' id='$k0'";
+                                if (is_bool($v0)) {
+                                    $checked = $v0 ? ' checked="checked"' : '';
+                                    echo " type='checkbox' value=true$checked onclick=this.toggleAttribute('checked') />";
+                                } else {
+                                    echo " type='text' value='$v0' />";
+                                }
+                            }
+                        }?>
                     </fieldset>
                 </td>
                 <td width="10%"><span class="remove btn btn-danger btn-sm">Remove</span></td>
             </tr>
-            <?php 
+            <?php
         foreach ($data->data->fields as $fkey => $field) {?>
             <tr class="trow">
                 <td>
@@ -91,16 +94,18 @@ class EditTable
                 </td>
                 <td width="10%"><span class="remove btn btn-danger btn-sm">Remove</span></td>
             </tr>
-            <?php 
-                    }
-                        ?>
+            <?php
+        }
+            ?>
         </tbody>
     </table>
     <?php
-                }
-                $roles = ['belongsTo', 'hasMany', 'hasManyAndBelongsTo'];
-                if (in_array($key, $roles)) {
-                    echo '<h4>' . $key . '</h4>';?>
+        } else {
+            $roles = ['belongsTo', 'hasMany', 'hasManyAndBelongsTo'];
+
+            if (in_array($key, $roles)) {
+
+                echo '<h4>' . $key . '</h4>';?>
     <table class="table table-warning table-striped table-sm wrapper">
         <thead>
             <tr>
@@ -108,85 +113,89 @@ class EditTable
             </tr>
         </thead>
         <tbody class="container ui-sortable" data-rf-row-count="0">
-            <tr class="template trow" style="display:none;">
+            <tr class="template trow">
                 <td class="col"><span class="move btn btn-info btn-sm"><i class="bi bi-arrow-down-up"></i></span></td>
                 <td>
                     <table>
 
                         <?php $data->$key = [];
-                            $data->$key[0] = new \Model\RelationDetails();
-                            if (!isset($data->$key[0]->relation)) {
-                                $data->$key[0]->relation = new \Model\Relation();
-                            }
+                $data->$key[0] = new \Model\RelationDetails();
+                if (!isset($data->$key[0]->relation)) {
+                    $data->$key[0]->relation = new \Model\Relation();
+                }
 
-                            foreach ($data->$key[0] as $rdKey => $rdValue) {
-                                if ($rdKey == 'relation') {
-                                    ?>
+                foreach ($data->$key[0] as $rdKey => $rdValue) {
+                    if ($rdKey == 'relation') {
+                        ?>
                         <tr>
                             <td><?php echo $rdKey ?>
                             </td>
                             <td><select name="table[<?=$key?>][{{row-count-placeholder}}][<?=$rdKey?>]">
                                     <option value=''></option>
                                     <?php echo "\n";
-                                    foreach ($this->relations as $r) {
-                                        $selected = $r->type == $key ? " selected='selected'" : '';
-                                        echo "<option value='$r->id'$selected'>{$r->type}</option>\n";
-                                    }
-                                    ?>
+                        foreach ($this->relations as $r) {
+                            $selected = $r->type == $key ? " selected='selected'" : '';
+                            echo "<option value='$r->id'$selected'>{$r->type}</option>\n";
+                        }
+                        ?>
 
                                 </select>
                                 <span><?php foreach ($rdValue as $attr => $val) {
-                                        echo "$attr: $val; ";
-                                    }?></span>
+                                    echo "$attr: $val; ";
+                                }?></span>
                             </td>
                         </tr>
                         <?php
-} else {
-                                    if (is_bool($rdValue)) {
-                                        $checked = $rdValue ? ' checked="checked"' : '';
-                                        echo "<tr><td>$rdKey</td><td><input type='checkbox' id='$rdKey' name='table[$key][{{row-count-placeholder}}][$rdKey]' value=true$checked /></td></tr>";
-                                    } else {
-                                        if ($rdKey == "id") {
-                                            echo "<input type='hidden' name='table[$key][{{row-count-placeholder}}][$rdKey]'>";
-                                        } else {
-                                            echo "<tr><td>$rdKey</td><td><input type='text' id='$rdKey' name='table[$key][{{row-count-placeholder}}][$rdKey]' value='$rdValue' /></td></tr>";
-                                        }
-                                    }
-                                }
+                    } else {
+                        if (is_bool($rdValue)) {
+                            $checked = $rdValue ? ' checked="checked"' : '';
+                            echo "<tr><td>$rdKey</td><td><input type='checkbox' id='$rdKey' name='table[$key][{{row-count-placeholder}}][$rdKey]' value=true$checked /></td></tr>";
+                        } else {
+                            if ($rdKey == "id") {
+                                echo "<input type='hidden' name='table[$key][{{row-count-placeholder}}][$rdKey]'>";
+                            } else {
+                                echo "<tr><td>$rdKey</td><td><input type='text' id='$rdKey' name='table[$key][{{row-count-placeholder}}][$rdKey]' value='$rdValue' /></td></tr>";
                             }
-                            ?>
+                        }
+                    }
+                }
+                ?>
                     </table>
                 </td>
                 <td width="10%"><span class="remove btn btn-danger btn-sm">Remove</span></td>
                 </td>
             </tr>
+
             <?php
-            foreach ($value as $av) {
-            foreach ($av as $rdKey => $rdValue) {
-            if (is_object($rdValue)) {
+        if (!empty($value)) {
+        foreach ($value as $av) {
             ?>
+
             <tr class="trow">
                 <td class="col"><span class="move btn btn-info btn-sm"><i class="bi bi-arrow-down-up"></i></span></td>
                 <td>
                     <table>
+                        <?php foreach ($av as $rdKey => $rdValue) { ?>
+                        <?php if (is_object($rdValue)) { ?>
                         <tr>
                             <td><?php echo $rdKey?></td>
                             <td><select name="<?php echo $rdKey?>">
-                                    <?php 
-                                foreach ($this->relations as $r) {
-                                            $selected = $rdValue == $r ? " selected='selected'" : '';
-                                    echo "<option value='$r->id'$selected'>{$r->type}</option>\n";
+
+                                    <?php
+                            foreach ($this->relations as $r) {
+                                $selected = $rdValue == $r ? " selected='selected'" : '';
+                                echo "<option value='$r->id'$selected'>{$r->type}</option>\n";
                             }
-                                ?>
+                            ?>
 
                                 </select>
                                 <span><?php foreach($rdValue as $attr => $val) {
-                                            echo "$attr: $val; ";
-                                        }?></span>
+                                    echo "$attr: $val; ";
+                                }?></span>
                             </td>
                         </tr>
-                        <?php 
-                         } else {
+                        <?php
+                        } else {
                             if (is_bool($rdValue)) {
                                 $checked = $rdValue ? ' checked="checked"' : '';
                                 echo "<tr><td>$rdKey</td><td><input type='checkbox' id='$rdKey' name='$rdKey' value=true$checked /></td></tr>";
@@ -198,23 +207,27 @@ class EditTable
                                 }
                             }
                         }
-                       
-                        ?>
+                    }
+
+        ?>
 
                     </table>
                 </td>
-                <td width="10%"><span class="remove btn btn-danger btn-sm">Remove</span></td>
+                <td width="10%"><span class="remove btn btn-danger btn-sm">Kustuta</span></td>
                 </td>
             </tr>
             <?php
-                    }
                 }
             }
         }
-    }
-?>
+                ?>
         </tbody>
     </table>
+    <?php
+
+        }
+    }
+ } ?>
     <input type="submit" value="Salvesta muudatused" />
 </form>
 <?php }
