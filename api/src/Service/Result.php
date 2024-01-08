01831aa1d6fa7b077533191d0765f267899878b2
Jaanus Nurmoja <jaanus.nurmoja@gmail.com>
2024-01-08 03:09:30 +0200

    hea mõte on ka uus fail lisada

---
+++ api/src/Service/Result.php
@@ -0,0 +1,21 @@
+<?php namespace Api\Service;
+
+use Api\Model\Entity;
+
+class Result extends QueryMaker
+{
+    public Entity $entity;
+    public array $resultsFromQuery;
+
+    public function __construct($tableName = null)
+    {
+        parent::__construct($tableName);
+    }
+
+    public function getData() {
+        $dbRead = new DbRead;
+        $query = $this->__toString();
+        $resultRows = $dbRead->anySelect($query);
+        return $resultRows;
+    }
+}
