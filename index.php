e6155f83720e3fef28d77b7e5b6aa693e20a5328
Jaanus Nurmoja <jaanus.nurmoja@gmail.com>
2024-02-13 04:23:50 +0200

    info mõõtmistulemuste kohta

--- index.php
+++ index.php
@@ -209,6 +209,8 @@ foreach (json_decode($rel) as $table => $params) {
                     lihtsamaid vahendeid ning ka vajalikke php-teeke.
                 </p>
                 <div>
+                    <h4 id="new" class="bg-warning">UUS (13.02.2024): Andmete laadimise aeg on kiirenenud ca 87 korda</h4>
+                    <p>Sarnaselt vanema koodiversiooniga on lisatud microtime(), et teha kindlaks, kui palju aega kulub päringu tegemisest tulemuste kuvamiseni</p>
                     <h4 id="new" class="bg-warning">UUS (05.02.2024): API väljund (sh json vormingus) on saanud soovitud kuju</h4>
                     <p>Võin öelda nii palju, et uue koodiga toimub päringu tulemuste kuvamine oluliselt kiiremini kui seni ning oluliselt lihtsamal moel (mis on ilmselt ka põhjus). Vajuta nupule "uus test" ja veendu.</p>
                     <p>VAEGTÖID siiski veel on. Eelkõige iseendaga suhtlev tabel products, mis ei anna seni veel teadmata põhjustel andmeid välja (ka vanas versioonis mitte, kuigi varem andis).</p>
